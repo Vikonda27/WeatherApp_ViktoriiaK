02067df34108d95362c84770ed621099c1154b2f
Vikonda27 <vikondaurova27@gmail.com>
2024-05-16 11:36:50 -0400

    adding action

--- WeatherApp_ViktoriiaK/WeatherViewController/Views/BottomBarView.swift
+++ WeatherApp_ViktoriiaK/WeatherViewController/Views/BottomBarView.swift
@@ -8,39 +8,34 @@
 import UIKit
 import SnapKit
 
-final class BottomBarView: UIView {
-    
+final class BottomBarView: MainView {
     private let deviderView = UIView()
     private let cityListButton = UIButton()
-    
+
     var cityListButtonAction: (() -> Void)?
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
+
+    override func setup() {
+        super.setup()
+
         backgroundColor = UIColor(named: "lightBlue")
         layer.shadowColor = UIColor.white.cgColor
         layer.shadowOpacity = 0.3
         layer.shadowRadius = 15
-        
+
         setupDeviderView()
         setupCityListButton()
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+
     private func setupDeviderView() {
         addSubview(deviderView)
         deviderView.backgroundColor = .white.withAlphaComponent(0.3)
-        
+
         deviderView.snp.makeConstraints { make in
             make.top.horizontalEdges.equalToSuperview()
             make.height.equalTo(1)
         }
     }
-    
+
     private func setupCityListButton() {
         addSubview(cityListButton)
         cityListButton.setImage(
@@ -49,6 +44,9 @@ final class BottomBarView: UIView {
             for: .normal
         )
         cityListButton.tintColor = .white
+        cityListButton.addAction(UIAction { _ in
+            self.cityListButtonAction?()
+        }, for: .touchUpInside)
 
         cityListButton.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(5)
